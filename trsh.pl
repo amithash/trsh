b5c1e8e2b0cb1cf8202f8ff1f0f644d16170243c
amithash <devnull@localhost>
2008-05-25 00:33:35 +0000

    made a bunch of bug fixes to setup, and trsh was overhauled to use functions and modular programming to do stuff and hance making it more maintainable.

--- trsh.pl
+++ trsh.pl
@@ -2,10 +2,10 @@
 
 #*************************************************************************
 # Copyright 2008 Amithash Prasad                                         *
+#									 *
+# this file is part of trsh.						 *
 #                                                                        *
-# This file is part of trsh                                              *
-#                                                                        *
-# Seeker is free software: you can redistribute it and/or modify         *
+# trsh is free software: you can redistribute it and/or modify           *
 # it under the terms of the GNU General Public License as published by   *
 # the Free Software Foundation, either version 3 of the License, or      *
 # (at your option) any later version.                                    *
@@ -25,9 +25,8 @@ use Getopt::Long;
 use File::Find;
 use Cwd;
 
-my $recover = 'NULL';
+my $recover = 0;
 my $empty = 0;
-my $remaining;
 my $view = 0;
 my $force = 0;
 my $undo = 0;
@@ -35,22 +34,26 @@ my $size = 0;
 my $help = 0;
 my $warn = 0;
 
+my $remaining;
+
 if (not defined $ENV{TRASH_DIR}) {
     print "The environment variable TRASH_DIR is not set\n";
     exit;
 }
 my $trash = $ENV{TRASH_DIR};
 my $history = "$ENV{TRASH_DIR}/.history";
-GetOptions( 'recover=s' => \$recover,
+GetOptions( 'recover' => \$recover,
             'empty'     => \$empty, 
             'view'      => \$view,
 	    'force'	=> \$force,
 	    'undo'	=> \$undo,
 	    'size'	=> \$size,
 	    'help'	=> \$help,
-	    'warn'	=> \$warn);	
+	    'interactive'=> \$warn,
+	    'warn'	=> \$warn);
 
 $remaining = join(' ', @ARGV);
+
 if( !(-e $trash) ) {
 	print "Could not find the trash directory, creating it...\n";
         system("mkdir $trash");
@@ -60,10 +63,12 @@ if( !(-e $history)){
 	system("touch $history");
 }
 
+
 if($help == 1){
 	usage();
 	exit;
 }
+
 
 if($size == 1){
 	open SZ,"du -sh $trash |";
@@ -73,156 +78,130 @@ if($size == 1){
 	exit;
 }
 
+# Restore the last deleted file
 if($undo == 1){
-	my $line;
-	my $last_line = '';
-	open HISTORY,"<$history" or die "Could not open the .history file in the TRSH DIR\n";
-	open HISTORY_NEW,"+>$history.new";
-	while ($line = <HISTORY>){
-		$last_line = $line;
-	}
-	chomp($last_line);
-	close HISTORY;
-	open HISTORY,"<$history" or die "Could not open the .history file in the TRSH DIR\n";
-	while($line = <HISTORY>){
-		chomp($line);
-		if($last_line ne $line){
-			print HISTORY_NEW "$line\n";
-		}
-	}
-	close HISTORY;
-	close HISTORY_NEW;
-	system("mv $history.new $history");
-	if($last_line eq ''){
-		print "History empty\n";
-		exit;
-	}
-	print "Restoring $last_line\n";
-	$recover = "$last_line";
+	restore_last_file();
+	exit;
 }
 
-
+# If the force flag is on, then rm instead of moving to trash.
 if($force == 1){
-	system("rm -rf $remaining");
-	exit;
-}
-
-if($view == 1){
-	system("ls $trash");
-	exit;
-}
-
-
-if($recover ne 'NULL'){
-	if($recover ne ''){
-		if(-e "$trash/$recover"){
-			my $i = 0;
-			while(-e "$trash/$recover\@$i"){
-				$i = $i + 1;
-			}
-			if($i == 0){
-				system("mv \'$trash/$recover\' .");
-			}
-			else{
-				$i = $i-1;
-				system("mv \'$trash/$recover\@$i\' \'$recover\'");
-			}
-		}
-		else{
-			print "Sorry cannot find $recover in the trash. Try rm -v to check if it exists\n";
-		}
+	# If the warn flag is on, delete interactively. 
+	if($warn == 1){
+		system("rm -ri $remaining");
 	}
 	else{
-		print "You must provide (a) file/s to recover\n";
-		exit;
+		system("rm -r $remaining");
 	}
+	exit;
 }
 
-elsif($empty == 1){
-	system ("rm -rf $history");
-	system ("rm -rf $trash/*");
-	system ("touch $history");
+# If the view flag is on, ls the files
+if($view == 1){
+	system("ls -l $trash");
+	exit;
 }
-else{
-	if($remaining ne ''){
-		my @rem = split(/\s/,$remaining);
-		my @not_there;
-		my $file_with_space = "";
-		my $file_name_with_space = "";
-		open HISTORY,">>$history" or die "Could not open the .history file in TRSH DIR\n";
-		foreach my $item (@rem){
-			my $i = 0;
+
+if($empty == 1){
+	if(get_response("Are you sure you want to empty the trash?") == 1){
+		system ("rm -rf $history");
+		system ("rm -rf $trash/*");
+		system ("touch $history");
+	}
+	exit;
+}
+
+
+if($remaining ne ''){
+	my @rem = split(/\s/,$remaining);
+	my @not_there;
+	my $file_with_space = "";
+	my $file_with_space_cmd = "";
+	foreach my $item (@rem){
+		my $i = 0;
+		if($recover == 1){
+			if(does_item_exist_in_history($item) > 0){
+				restore_file("$item");
+			}
+			else{
+				if($file_with_space == ""){
+					$file_with_space = $item;
+				}
+				else{
+					$file_with_space = $file_with_space . " " . $item;
+					$file_with_space_cmd = $file_with_space_cmd . "\\ " . $item;
+					if(does_item_exist_in_history($file_with_space_cmd) > 0){
+						restore_file($file_with_space_cmd);
+						$file_with_space = "";
+						$file_with_space_cmd = "";
+					}
+				}
+			}
+		}
+		# If we were dealing with a file with space... or so we think
+		elsif($file_with_space ne ""){
 			if(-e $item){
-				my @items = split(/\//,$item);
-				my $item_name = $items[$#items];
-				if(-d $item){
-					next unless(get_response($item_name) == 1);
+				print "WARN: $item exists, but I am looking for the rest of the file $file_with_space\n";
+				print "      If you wanted to delete $item, then $file_with_space does not exist!\n"
+			}
+			$file_with_space = $file_with_space . " " . $item;
+			$file_with_space_cmd = $file_with_space_cmd . "\\ " . $item;
+			if(-e $file_with_space){
+				if(-d $file_with_space){
+					if(get_response("Are you sure you want to delete directory: $file_with_space") == 1){
+						delete_file($file_with_space_cmd);
+					}
 				}
 				elsif($warn == 1){
-					next unless(get_response($item_name) == 1);
-				}
-				if(-e "$trash/$item_name"){
-					while(-e "$trash/$item_name\@$i"){
-						$i = $i + 1;
+					if(get_response("Are you sure you want to delete file: $file_with_space") == 1){
+						delete_file($file_with_space_cmd);
 					}
-					system("mv $item $trash/$item_name\@$i");
 				}
-				else{
-					system("mv $item $trash/$item_name");
+				else{	
+					delete_file($file_with_space_cmd);
 				}
-				print HISTORY "$item_name\n";
+				$file_with_space = "";
+				$file_with_space_cmd = "";
 			}
-			else{
-				my @items = split(/\//,$item);
-				my $item_name = $items[$#items];
-				if($file_with_space eq ""){
-					$file_with_space = $item;
-					$file_name_with_space = $item_name;
-				}
-				else{
-					$file_with_space = $file_with_space . " " . $item_name;
-					$file_name_with_space = $file_name_with_space . " " . $item_name;
-				}
-				if(-e "$file_with_space"){
-					if(-d "$file_with_space"){
-						if(get_response($file_with_space) == 0){
-							$file_with_space = "";
-							$file_name_with_space = "";
-							next;
-						}
-					}
-					elsif($warn == 1){
-						if(get_response($file_with_space) == 0){
-							$file_with_space = "";
-							$file_name_with_space = "";
-							next;
-						}
-					}
-					if(-e "$trash/$file_name_with_space"){
-						while(-e "$trash/$file_name_with_space\@$i"){
-							$i = $i + 1;
-						}
-						system("mv \'$file_with_space\' \'$trash/$file_name_with_space\@$i\'");
-					}
-					else{
-						system("mv \'$file_with_space\' \'$trash/$file_name_with_space\'");
-					}
-					print HISTORY "$file_name_with_space\n";
-					$file_with_space = "";
-					$file_name_with_space = "";
-				}
-			}	
 		}
-		if($file_with_space ne ""){
-			print "$file_with_space could not be deleted\n";
+		elsif(-e $item){  
+			if($file_with_space ne ""){
+				print "WARN: Cowardly refusing to delete imaginary file(s): $file_with_space\n";
+				$file_with_space = "";
+			}
+			if(-d $item){
+				next unless(get_response("Are you sure you want to delete directory: $item") == 1);
+			}
+			elsif($warn == 1){
+				next unless(get_response("Are you sure you want to delete file: $item") == 1);
+			}
+			delete_file("$item");
 		}
-		close HISTORY;
+		else{
+			# Either this file does not exist or it is the first part of a file name with space...
+			# So optimimistically starting that leg.
+
+			$file_with_space = $item;
+			$file_with_space_cmd = $item;
+		}	
 	}
-	else{
-		print "Nothing to be deleted\n";
-		exit;
+	if($file_with_space ne ""){
+		print "Could not delete $file_with_space\n";
 	}
-} 
+}
+else{
+	print "Gallantly deleted abslutely nothing!\n";
+	exit;
+}
+
+#####################################################
+# SUBS WHICH MAKE IT SUNNY OUTSIDE
+#####################################################
+
+#####################################################
+# MISL FUNCTIONS
+#####################################################
+
 sub usage{
 	print "Usage:
 
@@ -245,23 +224,148 @@ rm -f|-force FILE
 \tThis will permanently delete the FILE
 
 rm -s|-size
-\tThis will display the size of the trash folder.\n\n";
+\tThis will display the size of the trash folder.
+
+rm -i|-interactively|-w|-warn
+\tThis will nag the user for every file
+
+rm -h|-help
+\tThis will display this screen
+\n";
+
+
 }
 
 sub get_response{
-	my $item_name = shift;
+	my $message = shift;
 	my $ret = 0;
-	print "are you sure you want to remove $item_name? (y/n):";
+	print "$message (y/n):";
 	my $response = <STDIN>;
 	chomp($response);
-	while(not ($response eq "y" or $response eq "n")){
-		print "are you sure you want to remove $item_name? (y/n):";
-		$response = <STDIN>;
-		chomp($response);
-	}
-	if($response eq "y"){
+	# Any response, other than y,Y is considered as a n. 
+	if($response eq "y" or $response eq "Y"){
 		$ret = 1;
 	}
 	return $ret;
 }
+
+#############################################################
+# HIGH LEVEL TRASH FUNCTIONS
+#############################################################
+
+sub delete_file{
+	my $item = shift;
+	my @item_names = split(/\//, $item);
+	my $item_name = $item_names[$#item_names];
+	my $count = does_item_exist_in_history($item_name);
+	if($count == 0){
+		push_to_history($item_name);
+		system("mv $item $trash");
+	}
+	else{
+		push_to_history("$item_name\@$count");
+		system("mv $item $trash/$item\@$count");
+	}
+}
+
+sub restore_file{
+	my $item = shift;
+	my $cwd = cwd();
+	my $count = does_item_exist_in_history($item);
+	my $index = $count - 1;
+	if($count == 0){
+		print "Could not restore $item, it does not appear to exist in the trash\n";
+		return;
+	}
+	elsif($count == 1){
+		seek_and_destroy_in_history($item);
+		system("mv $trash/$item $cwd/$item");
+	}
+	else{
+		seek_and_destroy_in_history("$item\@$index");
+		system("mv $trash/$item\@$index $cwd/$item");
+	}
+}
+
+sub restore_last_file{
+	my $cwd = cwd();
+	my $item = pop_from_history();
+	my $item_cmd = join(" ", split(/\\\s/,$item));
+	if(-e "$trash/$item_cmd"){
+		if($item =~ /(.+)@\d+/){
+			print "Restoring $1...\n";
+			system("mv $trash/$item $cwd/$1");
+		}
+		else{
+			print "Restoring $item...\n";
+			system("mv $trash/$item $cwd/$item");
+		}
+	}
+	else{
+		print "Something is wrong... $item was in the history, but not in the trash... Raise a bug\n";
+	}
+}
+
+#############################################################
+# BASIC HISTORY FUNCTIONS
+#############################################################
+
+sub push_to_history{
+	my $item = shift;
+	my @contents = get_history();
+	$contents[$#contents+1] = $item;
+	make_history(@contents);
+}
+
+sub does_item_exist_in_history{
+	my $item = shift;
+	my $count = 0;
+	my @contents = get_history();
+	foreach my $i (@contents){
+		if($i =~ /^$item@\d+/){
+			$count++;
+		}
+		elsif($i =~ /^$item/){
+			$count++;
+		}
+	}
+	return $count;
+}
+
+sub pop_from_history{
+	my @contents = get_history();
+	make_history(@contents[0..($#contents-1)]);
+	return $contents[$#contents];
+}
+
+sub seek_and_destroy_in_history{
+	my $item_name = shift;
+	my @contents = get_history();
+	my $count = 0;
+	foreach my $i (@contents){
+		if($i eq "$item_name"){
+			my @new_countents = @contents[0..($count-1),($count+1)..$#contents];
+			make_history(@new_countents);
+			last;
+		}
+		$count++;
+	}
+}
+
+sub get_history{
+	open HIST, "$history" or die "Could not open history\n";
+	my @contents = split(/\n/, join("", <HIST>));
+	close(HIST);
+	return @contents;
+}
+
+sub make_history{
+	my @contents = @_;
+	system("rm $history");
+	open HIST,"+>$history" or die "Could not create history\n";
+	my $h = join("\n",@contents);
+	print HIST "$h";
+	close(HIST);
+}
+
 
