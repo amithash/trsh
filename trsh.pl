533185a54973d93227bffcc217c46b40f749b98d
amithash <devnull@localhost>
2009-12-13 21:38:46 +0000

    Deleted old trsh from svn.

--- trsh.pl
+++ trsh.pl
@@ -870,7 +870,7 @@ sub SetEnvirnment()
 sub Usage()
 {
 	print <<USAGE
-TRSH VERSION 3.3-288
+TRSH VERSION 3.3-289
 AUTHOR: Amithash Prasad <amithash\@gmail.com>
 
 USAGE: rm [OPTIONS]... [FILES]...
