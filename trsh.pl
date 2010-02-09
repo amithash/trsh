2d541e62e5dac256631dd3499c3c0f8b1bec73d1
amithash <devnull@localhost>
2010-02-08 18:40:54 -0700

    pointless commit to check username and pwd remember

--- trsh.pl
+++ trsh.pl
@@ -41,7 +41,7 @@ use Fcntl;
 use Term::ANSIColor;
 use Term::ReadKey;
 
-my $VERSION = "3.9-11";
+my $VERSION = "3.9-12";
 
 ##############################################################################
 #			   Function Declarations                             #
